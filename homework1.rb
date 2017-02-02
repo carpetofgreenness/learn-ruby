6148c4fe9eaae79cdaf099f930d07a02672270e3
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-02-02 10:34:20 -0500

    put some comments

--- homework1.rb
+++ homework1.rb
@@ -16,27 +16,32 @@ end
 
 puts findMax([100,-1000,10])
 
+#make a hash out of 2 arrays
+#array 1 will be the keys
+#array 2 will be the values
 def makeHash(array1,array2)
   superhash = Hash.new(0)
   for i in 0..array1.length-1
-    superhash[array1[i]] = array2[i]
+    superhash[array1[i]] = array2[i] #assigning key from array1 to value array2
   end
   return superhash
 end
 
 puts makeHash([:toyota,:tesla],["Prius","Model S"])
 
+#counts up to max and returns Fizz if divisible by 3,
+#Buzz if divisible by 5, and FizzBuzz if by both
 def fizzbuzz(max)
   for i in 0..max
-    iteration = ""
+    iteration = "" #start the count as empty
     if i%3==0
-      iteration << "Fizz"
+      iteration << "Fizz" #puts Fizz if div by 3
     end
     if i%5==0
-      iteration << "Buzz"
+      iteration << "Buzz" #puts Buzz if div by 5
     end
     if iteration == ""
-      iteration = i
+      iteration = i #if it was divisible by neither, just make it the num
     end
     puts iteration
   end
